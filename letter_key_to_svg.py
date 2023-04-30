7dd1b2143929f105d6d4cd1e795293ffa4352778
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-04-29 21:47:06 -0500

    Update letter_key_to_svg.py
    - Now a function, but still a bit weird and buggy.

--- letter_key_to_svg.py
+++ letter_key_to_svg.py
@@ -2,36 +2,66 @@ import json
 import os
 import shutil
 
-with open('dict.txt', 'r', encoding="utf-8") as f:
-    data = json.load(f)
 
-# print(data)
+def write_letter_file(output_location, file_text):
+    with open(output_location, 'w', encoding="utf-8") as text_file:
+        text_file.write(file_text)
 
-parent_dir = "svg_letters"
-letter_dir_src = "letters-1"
 
-if not os.path.exists(parent_dir):
-    os.makedirs(parent_dir)
-else:
-    shutil.rmtree(parent_dir)
+def generate_svg_folder(source_file, output_folder="svg_letters", input_glyphs="letters-1", split_folders=False):
+    # Open the text file with the data
+    with open(source_file, 'r', encoding="utf-8") as f:
+        data = json.load(f)
 
-# A-Z make folders and then add the glyphs
-for keys in data:
-    # Make the folder
-    this_folder = parent_dir + "/" + keys
-    os.makedirs(this_folder)
+    # print(data)
 
-    # String everything together and then output it as char.txt
-    full_string = ""
-    for char in data[keys]:
-        full_string = full_string + char
+    parent_dir = output_folder
+    letter_dir_src = input_glyphs
 
-    with open(this_folder+"/"+keys+".txt", 'w', encoding="utf-8") as text_file:
-        text_file.write(full_string)
+    if os.path.exists(parent_dir):
+        shutil.rmtree(parent_dir)
+    else:
+        os.makedirs(parent_dir)
 
-    # Now copy the symbols into it
-    this_letter = keys.split("_")[1]
-    print(this_letter)
+    if split_folders:
+        # A-Z make folders and then add the glyphs
+        for keys in data:
+            # Make the folder
+            this_folder = parent_dir + "/" + keys
+            os.makedirs(this_folder)
 
-    for char in data[keys]:
-        shutil.copy(letter_dir_src+"/"+this_letter+".svg", this_folder+"/"+char+".svg")
+            # String everything together and then output it as char.txt
+            full_string = ""
+            for char in data[keys]:
+                full_string = full_string + char
+
+            # Write a file to help test that the font will work.
+            write_letter_file(this_folder+"/"+keys+".txt", full_string)
+
+            # Now copy the symbols into the folder
+            this_letter = keys.split("_")[1]
+            print(this_letter)
+
+            for char in data[keys]:
+                shutil.copy(letter_dir_src+"/"+this_letter+".svg", this_folder+"/"+char+".svg")
+    # Toss everything into the main output folder
+    else:
+        for keys in data:
+            # String everything together and then output it as char.txt
+            full_string = ""
+            for char in data[keys]:
+                full_string = full_string + char
+
+            # Write a file to help test that the font will work.
+            write_letter_file(parent_dir + "/" + keys + ".txt", full_string)
+
+            # Now copy the symbols into the folder
+            this_letter = keys.split("_")[1]
+            print(this_letter)
+
+            for char in data[keys]:
+                shutil.copy(letter_dir_src + "/" + this_letter + ".svg", parent_dir + "/" + char + ".svg")
+
+
+if __name__ == '__main__':
+    generate_svg_folder("dict.txt", split_folders=False)
