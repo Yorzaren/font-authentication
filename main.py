26ecc325774849a05ba2dde8a9d9683f28a6b3e5
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-05-09 21:03:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-"""import zh
-from create_letter_key import generate_dict
+"""
+import font_generator.zh as zh
+import font_generator.cjk as cjk
+from font_generator.create_letter_key import generate_dict
+from font_generator.letter_key_to_svg import generate_svg_folder
 
 if __name__ == "__main__":
-    generate_dict(zh.ZH_ALL, "example/dict.txt")
+    folder = "CJK_D"
+    name = folder + ".txt"
+
+    generate_dict(cjk.CJK_D, name, scramble=True)
+    generate_svg_folder(name, output_folder=folder, input_glyphs="font_generator/letters", split_folders=False)
 """
