14d033e9cde1eba34dfababe5444c6d6ea0fd75a
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2023-05-14 15:21:33 -0500

    Renamed repo and updated references (#14)

--- tests/test_web.py
+++ tests/test_web.py
@@ -12,28 +12,28 @@ def test_index(app, client):
 def test_simple_correct_data(app, client):
     res = client.get("/simple")
     assert res.status_code == 200
-    assert b"Simple Validation" in res.data
+    assert b"Simple Authentication" in res.data
 
     with client.session_transaction() as session:
         this_code = session["simple_val"]
-        res = client.post("/simple_validation", data={"validation": this_code})
+        res = client.post("/simple_authentication", data={"authentication": this_code})
         assert res.status_code == 200
 
 
 def test_simple_incorrect_data(app, client):
     res = client.get("/simple")
     assert res.status_code == 200
-    assert b"Simple Validation" in res.data
+    assert b"Simple Authentication" in res.data
 
     with client.session_transaction() as session:
-        this_code = "BADCODE"  # Pass a value that isn't the correct validation code
+        this_code = "BADCODE"  # Pass a value that isn't the correct authentication code
         assert this_code != session["simple_val"]
-        res = client.post("/simple_validation", data={"validation": this_code})
+        res = client.post("/simple_authentication", data={"authentication": this_code})
         assert res.status_code == 302
 
 
-def test_simple_validation_endpoint(app, client):
-    res = client.get("/simple_validation")
+def test_simple_authentication_endpoint(app, client):
+    res = client.get("/simple_authentication")
     # The default should make this inaccessible
     # This only should be sent info and then evaluate it
     assert res.status_code == 405
@@ -45,28 +45,28 @@ def test_simple_validation_endpoint(app, client):
 def test_hard_correct_data(app, client):
     res = client.get("/hard")
     assert res.status_code == 200
-    assert b"Hard Validation" in res.data
+    assert b"Hard Authentication" in res.data
 
     with client.session_transaction() as session:
         this_code = session["hard_val"]
-        res = client.post("/hard_validation", data={"validation": this_code})
+        res = client.post("/hard_authentication", data={"authentication": this_code})
         assert res.status_code == 200
 
 
 def test_hard_incorrect_data(app, client):
     res = client.get("/hard")
     assert res.status_code == 200
-    assert b"Hard Validation" in res.data
+    assert b"Hard Authentication" in res.data
 
     with client.session_transaction() as session:
-        this_code = "BADCODE"  # Pass a value that isn't the correct validation code
+        this_code = "BADCODE"  # Pass a value that isn't the correct authentication code
         assert this_code != session["hard_val"]
-        res = client.post("/hard_validation", data={"validation": this_code})
+        res = client.post("/hard_authentication", data={"authentication": this_code})
         assert res.status_code == 302
 
 
-def test_hard_validation_endpoint(app, client):
-    res = client.get("/hard_validation")
+def test_hard_authentication_endpoint(app, client):
+    res = client.get("/hard_authentication")
     # The default should make this inaccessible
     # This only should be sent info and then evaluate it
     assert res.status_code == 405
@@ -76,28 +76,28 @@ def test_hard_validation_endpoint(app, client):
 def test_hardest_correct_data(app, client):
     res = client.get("/hardest")
     assert res.status_code == 200
-    assert b"Hardest Validation" in res.data
+    assert b"Hardest Authentication" in res.data
 
     with client.session_transaction() as session:
         this_code = session["hardest_val"]
-        res = client.post("/hardest_validation", data={"validation": this_code})
+        res = client.post("/hardest_authentication", data={"authentication": this_code})
         assert res.status_code == 200
 
 
 def test_hardest_incorrect_data(app, client):
     res = client.get("/hardest")
     assert res.status_code == 200
-    assert b"Hardest Validation" in res.data
+    assert b"Hardest Authentication" in res.data
 
     with client.session_transaction() as session:
-        this_code = "BADCODE"  # Pass a value that isn't the correct validation code
+        this_code = "BADCODE"  # Pass a value that isn't the correct authentication code
         assert this_code != session["hardest_val"]
-        res = client.post("/hardest_validation", data={"validation": this_code})
+        res = client.post("/hardest_authentication", data={"authentication": this_code})
         assert res.status_code == 302
 
 
-def test_hardest_validation_endpoint(app, client):
-    res = client.get("/hardest_validation")
+def test_hardest_authentication_endpoint(app, client):
+    res = client.get("/hardest_authentication")
     # The default should make this inaccessible
     # This only should be sent info and then evaluate it
     assert res.status_code == 405
